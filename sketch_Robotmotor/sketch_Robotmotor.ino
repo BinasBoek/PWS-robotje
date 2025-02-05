0a17785199b607f5d92f876334fbca1243c7125a
CodingBeavur <Butchbowy@gmail.com>
2025-02-05 09:57:35 +0100

    Robotmotor.ino

--- sketch_Robotmotor/sketch_Robotmotor.ino
+++ sketch_Robotmotor/sketch_Robotmotor.ino
@@ -1,12 +1,12 @@
 #include <Servo.h>
 
-// Define ultrasonic sensor pins
-#define TRIG_PIN 11       // Trigger pin of the HC-SR04
-#define ECHO_PIN 12       // Echo pin of the HC-SR04
+// US pins
+#define TRIG_PIN 11       
+#define ECHO_PIN 12       
 
-// Define motor control pins
-#define RIGHT_MOTOR_PIN 9 // Right motor (servo)
-#define LEFT_MOTOR_PIN 10 // Left motor (servo)
+//  motor pins
+#define RIGHT_MOTOR_PIN 9 
+#define LEFT_MOTOR_PIN 10 
 
 Servo rightMotor;
 Servo leftMotor;
@@ -15,76 +15,73 @@ long duration;
 int distance;
 
 void setup() {
-  // Initialize motors
+  // motors
   rightMotor.attach(RIGHT_MOTOR_PIN);
   leftMotor.attach(LEFT_MOTOR_PIN);
   
-  // Set up the HC-SR04 sensor
+  // Set up HC-SR04 sensor
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   
-  // Start the robot moving forward
+  // Start robot moving forward
   moveForward();
 }
 
 void loop() {
-  // Measure the distance using the ultrasonic sensor
   distance = measureDistance();
   
-  // If an object is detected closer than 10 cm
   if (distance < 10) {
-    stopMotors();         // Stop the robot
-    delay(500);           // Pause briefly
-    moveRight();          // Turn right for 0.8 seconds
+    stopMotors();         // Stop robot
+    delay(500);           // Pause
+    moveRight();          // rechts
     delay(800);
-    moveLeft();           // Turn left for 0.8 seconds
+    moveLeft();           // link
     delay(800);
-    moveForward();        // Resume moving forward
+    moveForward();        // moving forward
   } else {
-    // Continue moving forward if no obstacle is detected
+    // forward als er geen obstakel is
     moveForward();
   }
 }
 
-// Function to move forward (reversed directions)
+// Function move forward 
 void moveForward() {
-  rightMotor.write(180);  // Right motor moves forward
-  leftMotor.write(0);     // Left motor moves forward
+  rightMotor.write(180);  
+  leftMotor.write(0);     
 }
 
-// Function to stop motors
+// Function stop motors
 void stopMotors() {
-  rightMotor.write(90);   // Stop right motor
-  leftMotor.write(90);    // Stop left motor
+  rightMotor.write(90);   // Stop
+  leftMotor.write(90);    // Stop 
 }
 
-// Function to turn right
+// Function right
 void moveRight() {
-  rightMotor.write(0);    // Right motor moves backward
-  leftMotor.write(0);     // Left motor moves forward
+  rightMotor.write(0);    // backw
+  leftMotor.write(0);     // forw
 }
 
-// Function to turn left
+// Function left
 void moveLeft() {
-  rightMotor.write(180);  // Right motor moves forward
-  leftMotor.write(180);   // Left motor moves backward
+  rightMotor.write(180);  // forw
+  leftMotor.write(180);   // backw
 }
 
-// Function to measure the distance using the HC-SR04 sensor
+//  measure distance HC-SR04 sensor
 long measureDistance() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   
-  // Trigger the ultrasonic pulse
+  // Trigger ultrasonic pulse
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
   
-  // Measure the duration of the echo
+  // Measure duration echo
   duration = pulseIn(ECHO_PIN, HIGH);
   
-  // Calculate distance (distance = duration * speed of sound / 2)
-  // The speed of sound is approximately 343 meters per second at room temperature
+  
   distance = duration * 0.0344 / 2;
   
   return distance;
