2ace5dfff7926fc9e09236b1ade7658e7c79f65d
CodingBeavur <Butchbowy@gmail.com>
2025-02-05 10:21:23 +0100

    YAYHIJWERKT.ino

--- YAYHIJWERKT.ino
+++ YAYHIJWERKT.ino
@@ -14,12 +14,12 @@
 #define BT_TX_PIN 10
 #define BT_RX_PIN 11
 
-// Create instances for the servo motors and Bluetooth serial
+// Create instances  servo motors and Bluetooth serial
 Servo servoLeft;
 Servo servoRight;
 SoftwareSerial btSerial(BT_RX_PIN, BT_TX_PIN); 
 
-// Create instance for the LSM303DLHC sensor (accelerometer and compass)
+// Create instance for   LSM303DLHC sensor (accelerometer and compass)
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(12345);
 
 void setup() {
@@ -30,34 +30,34 @@ void setup() {
   servoLeft.attach(SERVO_LEFT_PIN);
   servoRight.attach(SERVO_RIGHT_PIN);
 
-  // Set the pin modes for the ultrasonic sensor
+  // Set   pin modes for   ultrasonic sensor
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
   // Initialize LSM303DLHC
   if (!accel.begin()) {
-    Serial.println("Could not initialize the accelerometer.");
+    Serial.println("Could not initialize   accelerometer.");
     while (1);
   }
 
   Serial.println("Robot is ready!");
 
-  // Initial positions for the servos (optional)
+  // Initial positions for   servos (optional)
   servoLeft.write(90); 
   servoRight.write(90);
 }
 
 void loop() {
   if (btSerial.available()) {
-    char incomingByte = btSerial.read(); // Read the incoming Bluetooth command
+    char incomingByte = btSerial.read(); // Read Bluetooth command
     Serial.print("Received Bluetooth command: ");
     Serial.println(incomingByte);
 
     // Control based on Bluetooth command
     if (incomingByte == 'F') {
       // Move forward
-      servoLeft.write(90);   // Set to neutral position (move forward)
-      servoRight.write(90);  // Set to neutral position (move forward)
+      servoLeft.write(90);   
+      servoRight.write(90);  
       Serial.println("Moving Forward");
     }
     else if (incomingByte == 'B') {
@@ -79,7 +79,7 @@ void loop() {
       Serial.println("Turning Right");
     }
     else if (incomingByte == 'S') {
-      // Stop - Set both servos to neutral (stop moving)
+      // Stop 
       servoLeft.writeMicroseconds(1511); 
       servoRight.writeMicroseconds(1511);
       Serial.println("Stopping");
